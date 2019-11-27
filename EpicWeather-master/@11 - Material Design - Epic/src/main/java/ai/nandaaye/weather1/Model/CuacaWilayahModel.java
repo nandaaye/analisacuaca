686bad1ccbaa58d5e449f0abad362ca3267d008b
nandaaye <nhandhaaye@gmail.com>
2019-11-27 21:24:34 +0700

    first commit

---
+++ EpicWeather-master/@11 - Material Design - Epic/src/main/java/ai/nandaaye/weather1/Model/CuacaWilayahModel.java
@@ -0,0 +1,12 @@
+package ai.nandaaye.weather1.Model;
+
+public class CuacaWilayahModel
+{
+	public PlaceModel place;
+	public CuacaModel model;
+
+	public CuacaWilayahModel(PlaceModel place, CuacaModel model) {
+		this.place=place;
+		this.model=model;
+	}
+}
